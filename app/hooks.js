b7900dd3da77ddaea19a44dc8babcf926993b835
CheckToSee <138322844+CheckToSee@users.noreply.github.com>
2023-09-09 13:06:30 -0400

    Initial commit

---
+++ app/hooks.js
@@ -0,0 +1,131 @@
+let boxCounter = 0;
+let rowCounter = 1;
+let answer = 'bison';
+
+export function keypress(keydown) {
+  let letter = keydown.key
+  letter = letter.toUpperCase()
+
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+
+  if (keydown.key == 'Backspace') {
+    deleteLetter()
+  } else if (keydown.key == 'Enter') {
+    submitLine()
+  } else if (boxCounter < 5 && rowCounter < 7) {
+    rowList[boxCounter].innerHTML = letter;
+    rowList[boxCounter].style.borderColor = '#565758';
+    console.log(boxCounter)
+    boxCounter++;
+  }
+}
+
+export function keyClick(keydown) {
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+  if (boxCounter < 5 && rowCounter < 7) {
+    rowList[boxCounter].innerHTML = keydown.target.value;
+    rowList[boxCounter].style.borderColor = '#565758';
+    boxCounter++;
+  } else {
+    console.log('out of range');
+  }
+}
+
+// IGNORE FOR NOW
+export function keyHighlight(keydown) {
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+  console.log(keydown.target.value);
+  if (keydown.target.innerHTML == '') {
+    return true;
+  }
+  return false;
+}
+
+export function deleteLetter() {
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+  if (boxCounter > 0) {
+    rowList[boxCounter - 1].innerHTML = '';
+    rowList[boxCounter - 1].style.borderColor = '#3A3A3C';
+    boxCounter--;
+  }
+}
+
+// FIX YELLOW ON EVERY LETTER
+export function submitLine() {
+  let guess = getLine();
+  guess = guess.toLowerCase();
+  let tileColor = '';
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+
+
+  if (guess == answer) {
+    gameWon()
+  } else if (rowCounter < 7 && boxCounter == 5) {
+    for (let i=0;i<5;i++) {
+      if (guess[i] == answer[i]) {
+        tileColor += 'g';
+      } else if (answer.includes(guess[i])) {
+        tileColor += 'y'
+      } else {
+        tileColor += 'n'
+      }
+    }
+    console.log(tileColor)
+
+    for (let j=0; j<5; j++) {
+      if (tileColor[j] == 'g') {
+        rowList[j].style.backgroundColor = '#538D4E';
+        rowList[j].style.borderColor = '#538D4E';
+      } else if (tileColor[j] == 'y') {
+        rowList[j].style.backgroundColor = '#B59F3B';
+        rowList[j].style.borderColor = '#B59F3B';
+      } else {
+        rowList[j].style.backgroundColor = '#3A3A3C';
+        rowList[j].style.borderColor = '#3A3A3C';
+      }
+    }
+    boxCounter = 0;
+    rowCounter++;
+  }
+}
+
+export function getLine() {
+  let guess = '';
+  const rowList = Array.from(
+    document.getElementsByClassName('row' + rowCounter),
+  );
+  for (let i = 0; i < boxCounter; i++) {
+    guess += rowList[i].innerHTML;
+  }
+  return guess;
+}
+
+export function resetGame() {
+  for (let i=1;i<7;i++) {
+    const rowList = Array.from(
+      document.getElementsByClassName('row' + i),
+    );
+    for (let i=0;i<5;i++) {
+      rowList[i].innerHTML = '';
+      rowList[i].style.backgroundColor = '#121213';
+      rowList[i].style.borderColor = '#3A3A3C';
+    }
+  }
+  boxCounter = 0;
+  rowCounter = 1;
+}
+
+export function gameWon() {
+  console.log('YOU WIN!')
+  resetGame()
+}
---
+++ components/Hero.jsx
@@ -0,0 +1,123 @@
+"use client";
+
+import React, { useState, useEffect } from 'react'
+import { RiDeleteBack2Line } from 'react-icons/ri'
+import * as logic from '../app/hooks.js'
+
+const Hero = () => {
+
+  useEffect(() => {
+    document.addEventListener('keydown', logic.keypress, true)
+  }, [])
+
+  const detectKeyDown = (e) => {
+    console.log("clicked key: ", e.key)
+  }
+
+  const [active, setActive] = useState(() => {
+    console.log("not active")
+    return false
+  })
+
+  // function switchActive() {
+  //   setActive(!active)
+  // }
+
+  if (active) {
+    logic.keypress('T')
+  }
+
+  return (
+    <div className='h-screen w-full mt-[-68px] flex justify-center bg-[#121213]'>
+      <div className='py-[68px] h-full flex flex-col items-center'>
+        {/* GUESS BOXES */}
+        <div className='grid grid-rows-6 gap-1 mt-3'>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className={`row1 h-14 w-14 border-2 flex justify-center items-center text-3xl font-bold ${ !logic.keyHighlight ? `border-[#565758]` : `border-[#3A3A3C]` }`}></div>
+            <div className='row1 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row1 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row1 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row1 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className='row2 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row2 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row2 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row2 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row2 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className='row3 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row3 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row3 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row3 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row3 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className='row4 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row4 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row4 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row4 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row4 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className='row5 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row5 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row5 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row5 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row5 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+          <div className='h-14 grid grid-cols-5 gap-1'>
+            <div className='row6 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row6 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row6 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row6 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+            <div className='row6 h-14 w-14 border-2 border-[#3A3A3C] flex justify-center items-center text-3xl font-bold'></div>
+          </div>
+        </div>
+
+        {/* KEYBOARD */}
+        <div className='flex flex-col mt-3 text-xl'>
+          <div className='flex flex-row'>
+            <button value="Q" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick} >Q</button>
+            <button value="W" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>W</button>
+            <button value="E" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>E</button>
+            <button value="R" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>R</button>
+            <button value="T" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>T</button>
+            <button value="Y" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>Y</button>
+            <button value="U" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>U</button>
+            <button value="I" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>I</button>
+            <button value="O" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>O</button>
+            <button value="P" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>P</button>
+          </div>
+          <div className='flex flex-row mt-2 justify-center'>
+            <button value="A" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>A</button>
+            <button value="S" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>S</button>
+            <button value="D" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>D</button>
+            <button value="F" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>F</button>
+            <button value="G" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>G</button>
+            <button value="H" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>H</button>
+            <button value="J" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>J</button>
+            <button value="K" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>K</button>
+            <button value="L" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>L</button>
+          </div>
+          <div className='flex flex-row mt-2 justify-center'>
+            <button className='bg-[#818384] w-16 h-14 rounded-[4px] font-bold ml-[6px] text-xs' onClick={logic.submitLine}>ENTER</button>
+            <button value="Z" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>Z</button>
+            <button value="X" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>X</button>
+            <button value="C" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>C</button>
+            <button value="V" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>V</button>
+            <button value="B" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>B</button>
+            <button value="N" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>N</button>
+            <button value="M" className='bg-[#818384] w-10 h-14 rounded-[4px] font-bold ml-[6px]' onClick={logic.keyClick}>M</button>
+            <button className='bg-[#818384] w-16 h-14 rounded-[4px] font-bold ml-[6px] flex justify-center items-center' onClick={logic.deleteLetter} >
+              <RiDeleteBack2Line size={22} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
